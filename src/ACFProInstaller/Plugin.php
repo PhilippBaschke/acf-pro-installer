ed262f25720b0be26078984b2879732515298b9f
Philipp Baschke <philipp@baschke.com>
2016-04-13 16:36:52 +0200

    Add Plugin and PluginTest classes
    The Travis CI build is failing because the src and tests folder are not
    available which causes composer lint to fail. A successful build is
    necessary to merge into master.

---
+++ src/ACFProInstaller/Plugin.php
@@ -0,0 +1,5 @@
+<?php namespace PhilippBaschke\ACFProInstaller;
+
+class Plugin
+{
+}
---
+++ tests/ACFProInstaller/PluginTest.php
@@ -0,0 +1,8 @@
+<?php namespace PhilippBaschke\ACFProInstaller\Test;
+
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFunction()
+    {
+    }
+}
