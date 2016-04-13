b0f511d4fff88a240887e1b379d86e2395763109
Philipp Baschke <philipp@baschke.com>
2016-04-13 17:45:42 +0200

    Implement PluginInterface in Plugin

--- src/ACFProInstaller/Plugin.php
+++ src/ACFProInstaller/Plugin.php
@@ -1,5 +1,25 @@
 <?php namespace PhilippBaschke\ACFProInstaller;
 
-class Plugin
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+/**
+ * A composer plugin that adds a repository for ACF PRO
+ *
+ * The WordPress plugin Advanced Custom Fields PRO (ACF PRO) does not
+ * offer a way to install it via composer natively.
+ *
+ * This plugin adds a 'package' repository to composer that downloads the
+ * correct version from the ACF site using the version number from
+ * composer.json and a license key from the ENVIRONMENT or an .env file.
+ *
+ * With this plugin user no longer need to supply the repository and expose
+ * their license key in composer.json.
+ */
+class Plugin implements PluginInterface
 {
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
 }
--- tests/ACFProInstaller/PluginTest.php
+++ tests/ACFProInstaller/PluginTest.php
@@ -1,8 +1,14 @@
 <?php namespace PhilippBaschke\ACFProInstaller\Test;
 
+use PhilippBaschke\ACFProInstaller\Plugin;
+
 class PluginTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFunction()
+    public function testImplementsPluginInterface()
     {
+        $this->assertInstanceOf(
+            'Composer\Plugin\PluginInterface',
+            new Plugin()
+        );
     }
 }
